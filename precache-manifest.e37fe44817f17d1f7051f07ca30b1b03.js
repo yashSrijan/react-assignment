f12317ea943201475b6f1999549deb33f91d27cb
yashSrijan <yarora@ex2india.com>
2019-08-22 21:02:18 +0530

    Updates

---
+++ precache-manifest.e37fe44817f17d1f7051f07ca30b1b03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6cf01440c726f6b8a500526f5ca0cc7",
+    "url": "/react-assignment/index.html"
+  },
+  {
+    "revision": "96b39d3ebc715255ab3b",
+    "url": "/react-assignment/static/css/main.39ba62a4.chunk.css"
+  },
+  {
+    "revision": "856c894c2bb7417d977c",
+    "url": "/react-assignment/static/js/2.a22bcecc.chunk.js"
+  },
+  {
+    "revision": "96b39d3ebc715255ab3b",
+    "url": "/react-assignment/static/js/main.2a27fa3b.chunk.js"
+  },
+  {
+    "revision": "13721714a70edaffc9f0",
+    "url": "/react-assignment/static/js/runtime~main.3005ea84.js"
+  }
+]);
